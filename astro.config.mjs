240ce8e4cb7a65a8e19b3d7d49f61a837cd65730
clpowe <clpowe@gmail.com>
2023-08-18 22:53:48 -0400

    serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import unocss from '@unocss/astro'
 import presetWind from '@unocss/preset-wind'
 import presetIcons from '@unocss/preset-icons'
 import presetTypography from '@unocss/preset-typography'
-import vercel from '@astrojs/vercel/edge'
+import vercelServerless from '@astrojs/vercel/serverless'
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: vercelEdge(),
+	adapter: vercelServerless(),
 	integrations: [
 		storyblok({
 			accessToken: env.STORYBLOK_TOKEN,
