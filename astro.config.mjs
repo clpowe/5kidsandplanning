9b24960ba60e218222d5f7742fcc73d5243c95ff
clpowe <clpowe@gmail.com>
2023-08-18 22:52:01 -0400

    edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ const env = loadEnv('', process.cwd(), 'STORYBLOK')
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: vercel(),
+	adapter: vercelEdge(),
 	integrations: [
 		storyblok({
 			accessToken: env.STORYBLOK_TOKEN,
