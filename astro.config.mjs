67dba1a15cda0326d3f483c6925dc3241cd546db
clpowe <clpowe@gmail.com>
2023-08-18 21:33:30 -0400

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,32 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import unocss from '@unocss/astro';
+import presetWind from '@unocss/preset-wind';
+import presetIcons from '@unocss/preset-icons';
+import presetTypography from '@unocss/preset-typography';
+import vercel from "@astrojs/vercel/serverless";
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
-import unocss from '@unocss/astro'
-import presetWind from '@unocss/preset-wind'
-import presetIcons from '@unocss/preset-icons'
-import presetTypography from '@unocss/preset-typography'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [
-		storyblok({
-			accessToken: env.STORYBLOK_TOKEN,
-			apiOptions: {
-				region: 'us'
-			}
-		}),
-
-		unocss({
-			injectReset: true,
-			presets: [presetWind(), presetIcons(), presetTypography()]
-		})
-	],
-	vite: {
-		plugins: [basicSsl()],
-		server: {
-			https: true
-		}
-	}
-})
+  output: 'server',
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    apiOptions: {
+      region: 'us'
+    }
+  }), unocss({
+    injectReset: true,
+    presets: [presetWind(), presetIcons(), presetTypography()]
+  })],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  },
+  adapter: vercel()
+});
